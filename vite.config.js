67f514ed26713336619f497f9704b98c16a56c35
radenaja <117440502+radenaja@users.noreply.github.com>
2026-04-20 14:13:56 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/raden-portfolio/',
+  base: '/Personal-Website-Portofolio/',
   publicDir: 'public',
 })
